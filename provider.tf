208dcb40bb04e8bffd98b9f6fdc1b16aa8a097ef
Sanat-gitH <sannatgiri1@gmail.com>
2026-07-01 01:11:15 +0530

    Refactor provider.tf for better formatting

--- provider.tf
+++ provider.tf
@@ -1,23 +1,21 @@
 terraform {
-    
-
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.28.0"
-    }
-backend "azurerm" {
-        resource_group_name = "infra-rg"
-        storage_account_name = "infrastg1"
-        container_name = "containerinfra"
-        key = "infra-pipeline.terraform.tfstate"
     }
   }
 
-
+  backend "azurerm" {
+    resource_group_name  = "infra-rg"
+    storage_account_name = "infrastg1"
+    container_name       = "containerinfra"
+    key                  = "infra-pipeline.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-    features {} 
-    subscription_id = "38840de6-dd80-4026-aeb6-0afb4905c320"
+  features {}
+
+  subscription_id = "38840de6-dd80-4026-aeb6-0afb4905c320"
 }
