b713506c542864629e74bfe6f7b8acf66f1666da
Sanat-gitH <sannatgiri1@gmail.com>
2026-07-01 01:07:28 +0530

    Re-add backend configuration for Azure storagetrr

--- provider.tf
+++ provider.tf
@@ -1,18 +1,20 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name = "infra-rg"
-        storage_account_name = "infrastg1"
-        container_name = "containerinfra"
-        key = "infra-pipeline.terraform.tfstate"
-    }
+    
 
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.28.0"
     }
+backend "azurerm" {
+        resource_group_name = "infra-rg"
+        storage_account_name = "infrastg1"
+        container_name = "containerinfra"
+        key = "infra-pipeline.terraform.tfstate"
+    }
   }
-  
+
+
 }
 
 provider "azurerm" {
